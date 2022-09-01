2b531bb26d4755684f2f0e683e2d9d5c610c97ba
Mastercuber <mr_w@posteo.de>
2022-09-02 00:26:19 +0200

    needed packages and explicitly setting output dir

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,9 @@ import { defineNuxtConfig } from 'nuxt'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   nitro: {
-    preset: './preset'
+    preset: './preset',
+    output: {
+      dir: '.output'
+    }
   }
 })
